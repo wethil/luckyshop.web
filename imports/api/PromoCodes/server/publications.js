e18a7174affa8ce1077ff065a36e35c8f16c556b
Fatih <ftdogru@yahoo.com>
2016-08-15 18:18:30 +0300

    Added basic api for credit and user infos

--- imports/api/PromoCodes/server/publications.js
+++ imports/api/PromoCodes/server/publications.js
@@ -4,20 +4,68 @@ import Branches from '../../branches/branches.js'
 
 
 
-/*Meteor.publish("allFoods",function(queryType,activeCooker,foodType){
-  switch (queryType) {
-    case 0:
-          return Foods.find({cookerId:activeCooker})
-      break;
-    case 1:
-          return Foods.find({foodType:foodType})
-        
-    default:
-         return  Foods.find()
-  }
+ var Api = new Restivus({
+    useDefaultAuth: true,
+    prettyJson: true
+  });
 
-})
-*/
+
+  Api.addRoute('user/:username', {authRequired: false}, {
+    get: function () {
+      return Meteor.users.findOne({username:this.urlParams.username},
+                                      {fields:{"services":0}})
+    },
+  });
+
+
+
+
+ Api.addRoute('createPromoCode/:id', {authRequired: false}, {
+    post: function () {
+      user =this.urlParams.id
+      promotion = this.bodyParams.promotion 
+     branches = Branches.find().fetch()
+     count = Branches.find().count()
+     nth = Math.floor(Math.random() * count);
+   
+    promotionCode = PromoCodes.insert({
+                      createdAt:new Date(),
+                      branchID:branches[nth]._id,
+                      promotion:promotion,
+                      createdBy:user
+                    })
+     
+      return PromoCodes.findOne({_id:promotionCode})
+    },
+  });
+
+
+   Api.addRoute('userCreditDec/:id', {authRequired: false}, {
+    post: function () {
+      user =this.urlParams.id
+      Meteor.users.update({_id:user},{$inc:{ 'profile.credit':-1 }})
+
+      return Meteor.users.findOne({_id:user},{fields:{"profile.credit":1}})
+    },
+  });
+
+
+Api.addRoute('userCreditInc/:id', {authRequired: false}, {
+    post: function () {
+      user =this.urlParams.id
+      Meteor.users.update({_id:user},{$inc:{ 'profile.credit':10 }})
+
+      return Meteor.users.findOne({_id:user},{fields:{"profile.credit":1}})
+    },
+  });
+
+
+
+
+
+
+
+
 Meteor.publishComposite('thisBranchesPromos', function(branch) {
     return {
         find: function() {
